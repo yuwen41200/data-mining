50533326bf2c045c1d68fb4ff9404980f1e6d954
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2017-10-12 23:24:43 +0800

    Bug fixes.

--- hw0/1-import-weather.py
+++ hw0/1-import-weather.py
@@ -5,53 +5,57 @@ import mysql.connector as mariadb
 import xml.etree.ElementTree as ElementTree
 
 if __name__ == '__main__':
-    cnx = mariadb.connect(user='root', database='dm')
+    cnx = mariadb.connect(user='root', host='localhost', database='dm', charset='utf8')
     cursor = cnx.cursor()
     tree = ElementTree.parse('../dataset/weather.xml')
     root = tree.getroot()
-    for loc in root.find('dataset').findall('location'):
-        sta = loc.find('locationName').text
-        for cat in loc.findall('weatherElement'):
-            if cat.find('elementName').text == '逐時觀測':
-                for row in cat.findall('time'):
-                    time = row.find('obsTime').text + ':00'  # YYYY-MM-DD HH:MM:SS
+    ns = {'cwb': 'urn:cwb:gov:tw:cwbcommon:0.1'}
+    for loc in root.find('cwb:dataset', ns).findall('cwb:location', ns):
+        sta = loc.find('cwb:locationName', ns).text
+        for cat in loc.findall('cwb:weatherElement', ns):
+            if cat.find('cwb:elementName', ns).text == '逐時觀測':
+                for row in cat.findall('cwb:time', ns):
+                    time = row.find('cwb:obsTime', ns).text + ':00'  # YYYY-MM-DD HH:MM:SS
+                    if time.endswith('24:00:00'):  # 24:00 is not allowed in MariaDB datetime
+                        time = time.replace('24:00:00', '23:59:59')  # change it to 23:59
                     p, t, h, v, d, r, s = None, None, None, None, None, None, None
-                    for col in row.findall('weatherElement'):
-                        if col.find('elementName').text == '測站氣壓':
-                            p = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '溫度':
-                            t = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '相對濕度':
-                            h = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '風速':
-                            v = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '風向':
-                            d = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '降水量':
-                            r = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '日照時數':
-                            s = col.find('elementValue').find('value').text
+                    for col in row.findall('cwb:weatherElement', ns):
+                        if col.find('cwb:elementName', ns).text == '測站氣壓':
+                            p = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '溫度':
+                            t = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '相對濕度':
+                            h = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '風速':
+                            v = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '風向':
+                            d = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '降水量':
+                            r = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                            r = None if r == 'T' else r  # some record has invalid value T
+                        elif col.find('cwb:elementName', ns).text == '日照時數':
+                            s = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
                         else:
-                            print('WTF?!', col.find('elementName').text)
+                            print('WTF?!', col.find('cwb:elementName', ns).text)
                     query = (
                         "INSERT INTO 逐時觀測 "
                         "(測站, 時間, 測站氣壓, 溫度, 相對濕度, 風速, 風向, 降水量, 日照時數) "
                         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
                     )
                     cursor.execute(query, (sta, time, p, t, h, v, d, r, s))
-            elif cat.find('elementName').text == '每日統計':
-                for row in cat.findall('time'):
-                    time = row.find('obsTime').text + ' 00:00:00'  # YYYY-MM-DD HH:MM:SS
+            elif cat.find('cwb:elementName', ns).text == '每日統計':
+                for row in cat.findall('cwb:time', ns):
+                    time = row.find('cwb:obsTime', ns).text + ' 00:00:00'  # YYYY-MM-DD HH:MM:SS
                     ht, lt, at = None, None, None
-                    for col in row.findall('weatherElement'):
-                        if col.find('elementName').text == '當日最高溫度(°C)':
-                            ht = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '當日最低溫度(°C)':
-                            lt = col.find('elementValue').find('value').text
-                        elif col.find('elementName').text == '當日平均溫度(°C)':
-                            at = col.find('elementValue').find('value').text
+                    for col in row.findall('cwb:weatherElement', ns):
+                        if col.find('cwb:elementName', ns).text == '當日最高溫度(°C)':
+                            ht = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '當日最低溫度(°C)':
+                            lt = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
+                        elif col.find('cwb:elementName', ns).text == '當日平均溫度(°C)':
+                            at = col.find('cwb:elementValue', ns).find('cwb:value', ns).text
                         else:
-                            print('WTF?!', col.find('elementName').text)
+                            print('WTF?!', col.find('cwb:elementName', ns).text)
                     query = (
                         "INSERT INTO 每日統計 "
                         "(測站, 時間, 當日最高溫度, 當日最低溫度, 當日平均溫度) "
@@ -59,7 +63,7 @@ if __name__ == '__main__':
                     )
                     cursor.execute(query, (sta, time, ht, lt, at))
             else:
-                print('WTF?!', cat.find('elementName').text)
+                print('WTF?!', cat.find('cwb:elementName', ns).text)
     cnx.commit()
     cursor.close()
     cnx.close()
